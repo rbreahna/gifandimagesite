a5922e27878c072ee16b8a1bf4ec6fb377d8595d
Radu Breahna <radu_breahna@yahoo.com>
2017-08-17 15:18:42 +0300

    AND

--- complete.php
+++ complete.php
@@ -6,13 +6,10 @@
 </head>
 <body>
 
-
-
-
 <?php
 require 'config/database.php';
 require 'functions.php';
-if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash']))
+if(isset($_GET['email']) && !empty($_GET['email']) && isset($_GET['hash']) && !empty($_GET['hash']))
 {
     // Verify data
     $email = test_input($_GET['email']); // Set email variable
@@ -26,7 +23,7 @@ $stmt->bindParam(2, $hash);
 $stmt->bindValue(3, '0');
 $stmt->execute();
 $count = $stmt->rowCount();
-if ($count ==1)
+if ($count == 1)
 {
 
 $stmt = $db->prepare("UPDATE users SET activated=? WHERE email = ? AND hash = ? AND activated = ?");
@@ -42,9 +39,6 @@ echo'<a href="login.php">Login</a>';
 //header("location:index.php");
 
 ?>
-
-
-
 
 
 <div class="alert-mesage">You will be redirected in </div>
